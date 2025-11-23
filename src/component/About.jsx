787bcf9fe2708987fa730a093b59f0f837bc6592
Kishor1345 <kishoredinesh0803@gmail.com>
2025-11-23 14:13:43 +0530

    resume download setting

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -52,7 +52,8 @@ const About = () => {
                 </p>
                 <div>
                   <a
-                    href="#About"
+                    href="https://drive.google.com/drive/u/0/folders/1FeWguctUtcq-ipD34x8IY731dqkm9nqS"
+                    target="_blank"
                     className=" btn w-25 text-white mt-2 "
                     style={{ backgroundColor: "#ae8af8ff", padding: "10px" }}
                   >
